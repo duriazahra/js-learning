234abec5fc5dcd39324fe2785cb6596aff398cbd
duriazahra <duriazahra8@gmail.com>
2026-08-03 08:12:39 +0000

    Number and Math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const num = 490
+
+// console.log(num);
+
+const numTwo = new Number(675)
+// console.log(numTwo);
+
+// console.log(numTwo.toString());
+// console.log(numTwo.toString().length);
+// console.log(numTwo.toFixed(2));
+
+const otherNum = 484.954
+// console.log(otherNum.toPrecision(2));
+
+const number = 1000000
+// console.log(number.toLocaleString());
+
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(2, 3, 4, 7));
+// console.log(Math.max(5, 7, 3, 2));
+
+// console.log(Math.random());
+// console.log((Math.random()* 10) + 1);
+// console.log(Math.floor(Math.random()* 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
